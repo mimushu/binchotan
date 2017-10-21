59a1e12ee9159cbd06c71d3890fa331361b862fc
Shu Mimura <shuu.mimura@gmail.com>
2017-10-21 21:18:22 +0900

    文言を変更・itemモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171021060931) do
+ActiveRecord::Schema.define(version: 20171021120030) do
+
+  create_table "items", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
